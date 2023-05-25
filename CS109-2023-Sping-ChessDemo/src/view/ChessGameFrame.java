de51a451e22a35b65f56304bd7aa2593c213accf
3746Lin <1847833323@qq.com>
2023-05-26 00:43:41 +0800

    添加更换风格按钮，不知道为啥换不了背景（第二次push）

--- CS109-2023-Sping-ChessDemo/src/view/ChessGameFrame.java
+++ CS109-2023-Sping-ChessDemo/src/view/ChessGameFrame.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 
 import controller.GameController;
 import model.Chessboard;
-import model.ChessboardPoint;
 import model.PlayerColor;
 
 import javax.swing.*;
@@ -12,6 +11,9 @@ import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * 这个类表示游戏过程中的整个游戏界面，是一切的载体
@@ -24,8 +26,12 @@ public class ChessGameFrame extends JFrame {
     private final int ONE_CHESS_SIZE;
 
     private ChessboardComponent chessboardComponent;
-    private JLabel statusLabel = new JLabel("CurrentPlayer:BLUE");
-    private JLabel TurnLabel = new JLabel("Turn:1");
+    private JLabel statusLabel = new JLabel("当前玩家:BLUE");
+    private JLabel TurnLabel = new JLabel("回合数:1");
+    private ImageIcon imageIcon1=new ImageIcon("CS109-2023-Sping-ChessDemo/resource/background.jpg");
+    private ImageIcon imageIcon2=new ImageIcon("CS109-2023-Sping-ChessDemo/resource/青青草原背景.jpg");
+    private ArrayList<JLabel> backgrounds=new ArrayList<JLabel>();
+    private int style=1;
     public ChessGameFrame(int width, int height) {
         setTitle("WHZ and LJZ's project"); //设置标题
         this.WIDTH = width;
@@ -48,6 +54,7 @@ public class ChessGameFrame extends JFrame {
         addTurnLabel();
         addCurrentPlayerLabel();
         addRegretButton();
+        addChangeStyleButton();
     }
 
     public ChessboardComponent getChessboardComponent() {
@@ -71,10 +78,10 @@ public class ChessGameFrame extends JFrame {
      * 在游戏面板中添加标签
      */
     private void addLabel() {
-        JLabel statusLabel = new JLabel("Options");
-        statusLabel.setLocation(HEIGTH+30, HEIGTH/10+60);
+        JLabel statusLabel = new JLabel("菜单");
+        statusLabel.setLocation(HEIGTH+70, HEIGTH/10+60);
         statusLabel.setSize(250, 60);
-        statusLabel.setFont(new Font("Rockwell", Font.BOLD, 30));
+        statusLabel.setFont(new Font("宋体", Font.BOLD, 30));
         add(statusLabel);
     }
 
@@ -83,19 +90,19 @@ public class ChessGameFrame extends JFrame {
      */
 
     private void addCheckRuleButton() {
-        JButton button = new JButton("Check Rule Here");
-        button.addActionListener((e) -> JOptionPane.showMessageDialog(this, "Basic Rule:Elephant>Lion>Tiger>Wolf>Dog>Cat>Rat(>Elephant)   "));
+        JButton button = new JButton("查看规则");
+        button.addActionListener((e) -> JOptionPane.showMessageDialog(this, "Basic Rule:Elephant>Lion>Tiger>Leopard>Wolf>Dog>Cat>Rat(>Elephant)   "));
         button.setLocation(HEIGTH-25, HEIGTH / 10 + 120);
         button.setSize(250, 60);
-        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        button.setFont(new Font("宋体", Font.BOLD, 24));
         add(button);
     }
     private void addInitialButton() {
-        JButton button = new JButton("Restart");
+        JButton button = new JButton("重新开始");
         button.addActionListener(new RestartButtonClickListener());
         button.setLocation(HEIGTH-25, HEIGTH / 10 + 200);
         button.setSize(250, 60);
-        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        button.setFont(new Font("宋体", Font.BOLD, 24));
         add(button);
     }
     private class RestartButtonClickListener implements ActionListener {
@@ -103,78 +110,162 @@ public class ChessGameFrame extends JFrame {
         public void actionPerformed(ActionEvent arg0) {
                 SwingUtilities.invokeLater(() -> {
                     chessboardComponent.getGameController().clearChessboard();
-                    chessboardComponent.registerController(new GameController(chessboardComponent, new Chessboard(),chessboardComponent.getGameController().getChessGameFrame()));
+                    chessboardComponent.initiateChessComponent(chessboardComponent.getGameController().getChessboard());
+                    chessboardComponent.registerController(new GameController(chessboardComponent,new Chessboard(),chessboardComponent.getGameController().getChessGameFrame()));
+                    chessboardComponent.getGameController().resetTurnAndCurrentPlayer();
+                    chessboardComponent.getGameController().RepaintAll(style);
+                    chessboardComponent.getGameController().getChessGameFrame().repaint();
                     chessboardComponent.repaint();
-                    addTurnLabel();
-                    addCurrentPlayerLabel();
                 });
         }
     }
     private void addBackground(){
         ((JPanel)this.getContentPane()).setOpaque(false);
         ImageIcon image;
-        image = new ImageIcon("CS109-2023-Sping-ChessDemo/resource/background.jpg");
+        if (style==1) {
+            image = imageIcon1;
+        }else {
+            image = imageIcon2;
+        }
+        if (backgrounds!=null&&backgrounds.size()!=0){
+            remove(backgrounds.get(0));
+            backgrounds.remove(0);
+        }
         JLabel background = new JLabel(image);
+        backgrounds.add(background);
         background.setBounds(0,0,this.getWidth(),this.getHeight());
         this.getLayeredPane().add(background,new Integer(Integer.MIN_VALUE));
     }
 
     public void addCurrentPlayerLabel() {
         if (chessboardComponent.getGameController() != null) {
-            JLabel ResetStatusLabel = new JLabel("CurrentPlayer:"+chessboardComponent.getGameController().getCurrentPlayer().toString());
+            JLabel ResetStatusLabel = new JLabel("当前玩家:"+chessboardComponent.getGameController().getCurrentPlayer().toString());
             remove(statusLabel);
             statusLabel = ResetStatusLabel;
         }
         statusLabel.setLocation(160, 55);
         statusLabel.setSize(180, 30);
-        statusLabel.setFont(new Font("Rockwell", Font.BOLD, 15));
+        statusLabel.setFont(new Font("宋体", Font.BOLD, 15));
         add(statusLabel);
     }
     public void addTurnLabel() {
         if (chessboardComponent.getGameController() != null) {
-            JLabel ResetTurnLabel = new JLabel("Turn:" + chessboardComponent.getGameController().getTurn());
+            JLabel ResetTurnLabel = new JLabel("回合数:" + chessboardComponent.getGameController().getTurn());
             remove(TurnLabel);
             TurnLabel = ResetTurnLabel;
         }
-        TurnLabel.setLocation(320, 55);
+        TurnLabel.setLocation(280, 55);
         TurnLabel.setSize(300, 30);
-        TurnLabel.setFont(new Font("Rockwell", Font.BOLD, 15));
+        TurnLabel.setFont(new Font("宋体", Font.BOLD, 15));
         add(TurnLabel);
     }
 
     private void addSaveButton() {
-        JButton button = new JButton("Save");
+        JButton button = new JButton("存档");
         button.setLocation(HEIGTH-25, HEIGTH / 10 + 280);
         button.setSize(250, 60);
-        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        button.setFont(new Font("宋体", Font.BOLD, 24));
         add(button);
 
         button.addActionListener((e) -> {
+            String input = JOptionPane.showInputDialog(this, "请输入要保存的文件名");
             String out = this.chessboardComponent.getGameController().save();
-            try {
-                FileWriter fileWriter = new FileWriter("save.txt");
-                BufferedWriter writer = new BufferedWriter(fileWriter);
-                writer.write(out);
-                writer.close();
-                fileWriter.close();
-            } catch (IOException ee) {
-                ee.printStackTrace();
+            while (input!=null){
+                try {
+                    if (input.equals("")) {
+                        JFrame frame = new JFrame("提示窗口");
+                        frame.setSize(400, 300);
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        JOptionPane.showMessageDialog(frame, "文件名为空，请重新输入文件名");
+                        input = JOptionPane.showInputDialog(this, "请输入要保存的文件名");
+                        remove(frame);
+                    }else {
+                        FileWriter fileWriter = new FileWriter("CS109-2023-Sping-ChessDemo/txt/" + input + ".txt");
+                        BufferedWriter writer = new BufferedWriter(fileWriter);
+                        writer.write(out);
+                        writer.close();
+                        fileWriter.close();
+                        break;
+                    }
+                } catch (IOException ee) {
+                    ee.printStackTrace();
+                }
             }
         });
     }
 
     private void addLoadButton() {
-        JButton button = new JButton("Load");
+        JButton button = new JButton("读档");
         button.setLocation(HEIGTH-25, HEIGTH / 10 + 360);
         button.setSize(250, 60);
-        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        button.setFont(new Font("宋体", Font.BOLD, 24));
         add(button);
-
         button.addActionListener(e -> {
-            this.chessboardComponent.getGameController().load("save.txt");
+            String input = JOptionPane.showInputDialog(this, "请输入要载入的文件名");
+            while (input!=null) {
+                try {
+                    File txt = new File("CS109-2023-Sping-ChessDemo/txt/" + input + ".txt");
+                    if (txt.exists()) {
+                        this.chessboardComponent.getGameController().load("CS109-2023-Sping-ChessDemo/txt/" + input + ".txt");
+                        break;
+                    } else if (input.equals("")) {
+                        JFrame frame = new JFrame("提示窗口");
+                        frame.setSize(400, 300);
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        JOptionPane.showMessageDialog(frame, "文件名为空，请重新输入文件名");
+                        input = JOptionPane.showInputDialog(this, "请输入要保存的文件名");
+                        remove(frame);
+                    } else {
+                        throw new FileNotFoundException("文件不存在");
+                    }
+                } catch (FileNotFoundException exception) {
+                    JFrame frame = new JFrame("提示窗口");
+                    frame.setSize(400, 300);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    JOptionPane.showMessageDialog(frame, "文件不存在，请重新输入文件名");
+                    input = JOptionPane.showInputDialog(this, "请输入要保存的文件名");
+                    remove(frame);
+                }
+            }
         });
     }
+    private void addChangeStyleButton() {
+        JButton button = new JButton("更换风格");
+        button.setLocation(HEIGTH-25, HEIGTH / 10 + 440);
+        button.setSize(250, 60);
+        button.setFont(new Font("宋体", Font.BOLD, 24));
+        add(button);
 
+        button.addActionListener(e ->  {
+            showChangeStyleOptions();
+        });
+
+    }
+    private void showChangeStyleOptions(){
+        JFrame jFrame = new JFrame("选择风格");
+        jFrame.setSize(400, 100);
+        jFrame.setLocation(600,450);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JLabel jLabel=new JLabel("选择你想要的风格");
+        JButton normal = new JButton("普通风格");
+        JButton happysheep = new JButton("喜羊羊与灰太狼");
+        JPanel jPanel = new JPanel();
+        jPanel.add(jLabel);
+        jPanel.add(normal);
+        jPanel.add(happysheep);
+        jFrame.getContentPane().add(jPanel, BorderLayout.CENTER);
+        jFrame.setVisible(true);
+        normal.addActionListener(e -> {
+            style=1;
+            chessboardComponent.getGameController().RepaintAll(1);
+            chessboardComponent.getGameController().getChessGameFrame().addBackground();
+        });
+        happysheep.addActionListener(e -> {
+            style=2;
+            chessboardComponent.getGameController().RepaintAll(2);
+            chessboardComponent.getGameController().getChessGameFrame().addBackground();
+        });
+    }
     private void addRegretButton() {
         JButton button = new JButton("Regret");
         button.setLocation(HEIGTH-25, HEIGTH / 10 + 440);
@@ -186,5 +277,4 @@ public class ChessGameFrame extends JFrame {
             this.chessboardComponent.getGameController().regret();
         });
     }
-
 }
